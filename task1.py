a27be68160106306530b77684ef4fb31a7733b18
Лаврова Елизавета Александровна <e.lavrova2@main.nsu.ru>
2024-10-23 13:46:52 +0700

    1, 5

--- task1.py
+++ task1.py
@@ -6,3 +6,30 @@
 # 4.YES, если список содержит трехзначное число, состоящее из одинаковых цифр
 # NO в противном случае;
 # 5.список с удаленными первым и последним элементами.
+
+from random import randint
+
+def find(x):
+    for i in x:
+        if 100 <= i <= 999:
+            if i % 10 == i // 100 and i % 10 == (i // 10 % 10):
+                return True
+    return False
+
+n = int(input())
+lst = [randint(0, 1000) for _ in range(n)]
+#print(lst)
+
+print(len(lst))
+print(lst[-1])
+print(lst[::-1])
+if find(lst):
+    print('YES')
+else:
+    print('NO')
+
+lst.pop(0)
+lst.pop(-1)
+print(lst)
+
+
