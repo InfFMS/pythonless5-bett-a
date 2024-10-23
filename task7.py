fc8c560b333cafd2ea88ed7b3f8e31a30cb8a2c5
Лаврова Елизавета Александровна <e.lavrova2@main.nsu.ru>
2024-10-23 14:06:58 +0700

    7

--- task7.py
+++ task7.py
@@ -4,3 +4,21 @@
 # Пример: ввод N = 6
 # [20, -90, 15, -34, 10, 0]
 # Вывод: [20, 15, 10, -90, -34, 0]
+
+import random
+
+lst = [random.randint(-100, 100) for _ in range(int(input()))]
+print(lst)
+pos = []
+neg = []
+zero = 0
+for i in lst:
+    if i == 0:
+        zero += 1
+    elif i > 0:
+        pos.append(i)
+    else:
+        neg.append(i)
+
+lst = pos + neg + [0] * zero
+print(lst)
