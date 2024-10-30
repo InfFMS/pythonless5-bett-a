9dd68a1c3559fa941e5671cf7dc634c45a809a43
Лаврова Елизавета Александровна <e.lavrova2@main.nsu.ru>
2024-10-30 12:38:13 +0700

    9

--- task9.py
+++ task9.py
@@ -3,3 +3,13 @@
 # Пример: ввод N = 8
 # [10, 20, 10, 20, 30, 40, 30, 50]
 # После удаления дублей:  [10, 20, 30, 40, 50]
+
+import random
+lst = [random.randint(0,50) for _ in range(int(input()))]
+new_lst = []
+for i in lst:
+    if not(i in new_lst):
+        new_lst.append(i)
+
+print(lst)
+print(new_lst)
